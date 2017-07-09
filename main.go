51acecc52e196e65aea9f8474fa24922e2432dfb
Andrew Sharp <andrew.sharp57@gmail.com>
2017-07-09 15:29:36 -0500

    weather

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strings"
+)
+
 func main() {
-	println("hello!")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/weather/", weather)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
+
+func weather(w http.ResponseWriter, r *http.Request) {
+	city := strings.SplitN(r.URL.Path, "/", 3)[2]
+	data := weatherData{city, rand.Float64() * 100}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+type weatherData struct {
+	Name string
+	Fahrenheit float64
 }
