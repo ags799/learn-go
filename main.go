0278f47aa32422539ceeed4ee1db439854a8ee37
Andrew Sharp <andrew.sharp57@gmail.com>
2017-07-09 20:09:12 -0500

    remove http framework

--- main.go
+++ main.go
@@ -8,23 +8,5 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/weather/", weather)
-	http.ListenAndServe(":8080", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
-}
-
-func weather(w http.ResponseWriter, r *http.Request) {
-	city := strings.SplitN(r.URL.Path, "/", 3)[2]
-	data := weatherData{city, rand.Float64() * 100}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(data)
-}
-
-type weatherData struct {
-	Name string
-	Fahrenheit float64
+	print("Hello, world!")
 }
