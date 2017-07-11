6dc616161c7313aea30bf775f51aa13694f82b5b
Andrew Sharp <andrew.sharp57@gmail.com>
2017-07-10 19:37:46 -0500

    list returns one placeholder item

--- main.go
+++ main.go
@@ -4,8 +4,17 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+	r.GET("/item", list)
 	r.GET("/ping", ping)
 	r.Run()
+}
+
+type Item struct {
+	Description string `json:"description"`
+}
+
+func list(c *gin.Context) {
+	c.JSON(200, Item{Description: "some description"})
 }
 
 func ping(c *gin.Context) {
