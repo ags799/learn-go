39075461513ed7dea0e26d312145531887290874
Andrew Sharp <andrew.sharp57@gmail.com>
2017-07-16 12:54:56 -0500

    connecting to postgres docker

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	r := gin.Default()
+	db, err := sql.Open("postgres", "user=postgres sslmode=disable")
+	if err != nil {
+		print("Open: " + err.Error())
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		print("Ping: " + err.Error())
+		return
+	}
+	err = db.Close()
+	if err != nil {
+		print("Close: " + err.Error())
+		return
+	}
+	print("success")
+	/*r := gin.Default()
 	r.GET("/item", list)
 	r.GET("/ping", ping)
-	r.Run()
+	r.Run()*/
 }
 
 type Item struct {
